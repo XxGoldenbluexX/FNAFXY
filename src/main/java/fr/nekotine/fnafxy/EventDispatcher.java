51179ae6801c50ab70a1c1ee5f76bd0380e97c0d
XxGoldenbluexX <tonyg222@outlook.fr>
2022-02-21 12:07:44 +0100

    Ajout des dépendances + classe principale + event dispatcher

---
+++ src/main/java/fr/nekotine/fnafxy/EventDispatcher.java
@@ -0,0 +1,7 @@
+package fr.nekotine.fnafxy;
+
+import org.bukkit.event.Listener;
+
+public class EventDispatcher implements Listener {
+
+}
---
+++ src/main/java/fr/nekotine/fnafxy/FNAFXY.java
@@ -0,0 +1,20 @@
+package fr.nekotine.fnafxy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FNAFXY extends JavaPlugin {
+
+	public static FNAFXY instance;
+	
+	private EventDispatcher dispatcher;
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		instance = this;
+		dispatcher = new EventDispatcher();
+		Bukkit.getPluginManager().registerEvents(dispatcher, instance);
+	}
+	
+}
